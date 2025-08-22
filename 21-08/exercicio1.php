d969c7e7d397ad54425dbebd7d8614a4cd1a9275
Gabriel <hamburguer117@gmail.com>
2025-08-21 22:02:56 -0300

    lista1

--- 21-08/exercicio1.php
+++ 21-08/exercicio1.php
@@ -6,10 +6,12 @@
 <title>Soma valores</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
+
 <body> 
+
 <div class="container">
 <h1>Soma valores</h1>
-<form method="post" action="resposta.php">
+<form method="post">
 <div class="mb-3">
               <label for="valor" class="form-label">Valor 1</label>
               <input type="number" id="valor" name="valor1" class="form-control" required="">
@@ -19,6 +21,18 @@
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+
+<?php 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    echo "A soma dos valores é: " . $valor1 + $valor2   ;
+    // -  +  *  **  /  //  % 
+    }
+?>
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
