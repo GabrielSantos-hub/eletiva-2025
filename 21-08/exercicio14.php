2443bcc7025623b749bbef68b54414465ef6f5ae
Gabriel <hamburguer117@gmail.com>
2025-09-04 16:49:31 -0300

    save

--- 21-08/exercicio14.php
+++ 21-08/exercicio14.php
@@ -21,11 +21,6 @@
         $km = $_POST['km'];
         echo "A quilometragem convertida em milhas é: ". ($km * 0.621371);
 ?>
-
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
